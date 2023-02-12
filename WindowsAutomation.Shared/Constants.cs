19a834224a433311eddb61b60b834fb83793b9e2
Ylniss <zupqa0@gmail.com>
2023-02-12 21:33:07 +0100

    init commit

---
+++ WindowsAutomation.Shared/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace WindowsAutomation.Shared;
+
+public static class Constants
+{
+    public static class Paths
+    {
+        public const string Repo = "C:/Repo";
+        public const string Software = "C:/Downloads/Software";
+        public const string ProjectPath = $"{Repo}/WindowsAutomation.InitSetuper/Software";
+    }
+    
+    public const string ProfileName = "Microsoft.PowerShell_profile.ps1";
+}
